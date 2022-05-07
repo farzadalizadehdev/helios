b743abe17076c40fb7111f1a24e06a4c5a70818f
farzadalizadehdev <farzadalizadehdev@gmail.com>
2022-05-07 23:05:18 +0430

    import tailwind css

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,4 +1,13 @@
+import { Card,Button } from "antd";
 const Dashboard = (props) => {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <div className="p-6 rounded-lg w-1/3 bg-gradient-to-r from-cyan-100 to-blue-100">
+        <h3 className="text-lg">ثبت پزشک</h3>
+        <p className="font-normal">نمایش پیام مناسب به کاربر</p>
+        <Button className="mt-4" shape="round" size="middle" type="primary">ثبت پزشک</Button>
+      </div>
+    </div>
+  );
 };
 export default Dashboard;

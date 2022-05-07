0adb5c51a0634b4435dfd0c77aaa00a16f18f290
farzadalizadehdev <farzadalizadehdev@gmail.com>
2022-05-07 20:27:04 +0430

    fix lazy loading

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,16 @@
-import React, { Suspense } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-// import LazyLoading from "./shared/components/LazyLoading/LazyLoading";
-// const Auth = React.lazy(() => import("./containers/Auth/Auth"));
-// const Dashboard = React.lazy(() => import("./containers/Dashboard/Dashboard"));
-// const Requests = React.lazy(() => import("./containers/Requests/Requests"));
-import Dashboard from "./containers/Dashboard/Dashboard";
-import Requests from "./containers/Requests/Requests";
+import React, { Suspense } from 'react';
+import { Routes, Route } from "react-router-dom";
+const Dashboard = React.lazy(() => import("./containers/Dashboard/Dashboard"));
+const Requests = React.lazy(() => import("./containers/Requests/Requests"));
 
 const RoutesWithNavigation = () => {
   return (
-    // <Suspense fallback={<LazyLoading />}>
-    <Routes>
-      {/* <Route path="/" element={<Auth />} /> */}
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/requests" element={<Requests />} />
-    </Routes>
-    // </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/requests" element={<Requests />} />
+      </Routes>
+    </Suspense>
   );
 };
 
